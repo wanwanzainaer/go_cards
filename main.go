8f71d5fa6c5855117ce972803ce0de0551cba861
Wanwan <wanwanzainaer@gmail.com>
2020-07-21 12:56:11 -0700

    Add for loop for cards

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	fmt.Println(card)
+	// card := "Ace of Spades"'
+	// card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
-func newCard() {
-
+func newCard() string {
+	return "Five of Diamonds"
 }
